09aac1896ed8ed8711b36b70bd84be61704c6e85
Michael Weems <mweems5@gmail.com>
2024-10-09 17:53:50 -0500

    add golden quicksort

--- quicksort_golden.c
+++ quicksort_golden.c
@@ -1,5 +1,35 @@
 #include "quicksort.h"
 
-void quicksort(int *arr, int arrSize) {
+int partition(int *arr, int lo, int hi) {
+	int pivot = arr[hi];
+	int idx = lo - 1;
 
+	for (int i = lo; i <= hi - 1; ++i) {
+		if (arr[i] < pivot) {
+			++idx;
+			int tmp = arr[i];
+			arr[i] = arr[idx];
+			arr[idx] = tmp;
+		}
+	}
+	++idx;
+
+	arr[hi] = arr[idx];
+	arr[idx] = pivot;
+	return idx;
+}
+
+void qs(int *arr, int lo, int hi) {
+	if (lo >= hi) {
+		return;
+	}
+
+	int p = partition(arr, lo, hi);
+
+	qs(arr, lo, p-1);
+	qs(arr, p+1, hi);
+}
+
+void quicksort(int *arr, int arrSize) {
+	qs(arr, 0, arrSize - 1);
 }
