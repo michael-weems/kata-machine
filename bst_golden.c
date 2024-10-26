221b31acace772bcc34f5f9b18ca0d8c27f9b4e9
Michael Weems <mweems5@gmail.com>
2024-10-26 15:51:13 -0500

    bst golden setup, still need to do it

--- bst_golden.c
+++ bst_golden.c
@@ -1,2 +1,18 @@
 #include <stdio.h>
 #include "bst.h"
+
+struct btnode *btnode_create(int key) {
+	
+}
+void btnode_free(struct btnode *root) {
+	
+}
+
+void bst_insert(struct btnode *root, int key) {
+
+}
+void bst_delete(struct btnode *root, int key) {
+
+}
+bool bst_search(struct btnode *root, int key) {
+}
